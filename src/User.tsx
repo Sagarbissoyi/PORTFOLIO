6fdfd1e86ff8f6575ebcfe28a283ed7eb929f34d
sagarbissoyi <sagarbissoyi10@gmail.com>
2024-09-26 21:33:55 +0530

    fix error

--- src/User.tsx
+++ src/User.tsx
@@ -71,7 +71,7 @@ const ProjectInfo = [
 const SkillInfo = [
     {
         title: "Frontend",
-        skills: ["html", "CSS","JavaScript", "React JS", "Redux", "Tailwind CSS","Bootstrap"
+        skills: ["HTML", "CSS","JavaScript", "React JS", "Redux", "Tailwind CSS","Bootstrap"
         ]
     },
     {
