4da5de9e2608653b651d3cc260736b3a8dfc7ef7
sagarbissoyi <sagarbissoyi10@gmail.com>
2024-11-24 23:39:56 +0530

    add new project

--- src/User.tsx
+++ src/User.tsx
@@ -10,13 +10,13 @@ const Info={
 
 const ProjectInfo = [
     {
-        title: "Ekart E-comm App",
-        desc: "Ekart is a modern, fully responsive e-commerce platform built using React, TailwindCSS, Heroicons, and HeadlessUI, offering a seamless shopping experience across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add items to a wishlist or remove them, search for specific products, and add them to a shopping cart for purchase. The checkout process includes form validation for address and payment information, ensuring accuracy and security. After purchase, users can view their order history in the Orders section. Ekart combines robust functionality with a sleek, intuitive design, making online shopping effortless and enjoyable.",
-        image: "Ekart.png",
+        title: " Audio Platform PODCASTER",
+        desc: " Audio Platform PODCASTER, fully responsive Podcaster platform built using React, Node JS, Express JS, Mongo DB and Tailwind CSS, offering a seamless listening podcast across all devices. It features secure user authentication with login, registration, and logout functionalities. Users can add podcast and listeing, ",
+        image: "Podcaster.png",
         live: true,
-        technologies: ["React", "Tailwind", "Redux", "Heroicons", "HeadlessUI"],
-        link: "https://code-mars.github.io/Ekart/",
-        github: "https://github.com/Code-Mars/Ekart/"
+        technologies: ["React", "Node JS", "Express JS", "Mongo DB" , "Tailwind CSS"],
+        link: "https://podcast-frontend-seven.vercel.app/",
+        github: "https://github.com/Sagarbissoyi/PODCAST-FRONTEND"
     },
     {
         title: "Air Travel",
